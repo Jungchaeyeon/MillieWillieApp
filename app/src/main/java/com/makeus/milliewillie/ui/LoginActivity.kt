00793eb52e7c71f380724317868a45a945edad06
Jungchaeyeon <chaeyeon1697@gmail.com>
2021-03-04 20:58:37 +0900

    구글 로그인 연동

--- app/src/main/java/com/makeus/milliewillie/ui/LoginActivity.kt
+++ app/src/main/java/com/makeus/milliewillie/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.makeus.milliewillie.ui
 
+import android.content.Intent
 import com.makeus.base.activity.BaseDataBindingActivity
 import com.makeus.milliewillie.ActivityNavigator
 import com.makeus.milliewillie.R
@@ -12,19 +13,41 @@ class LoginActivity : BaseDataBindingActivity<ActivityLoginBinding>(R.layout.act
 
     private val viewModel by viewModel<LoginViewModel>()
 
+    private val requestGoogleAuth = 9001
+
     override fun ActivityLoginBinding.onBind() {
         vi = this@LoginActivity
         vm = viewModel
         viewModel.bindLifecycle(this@LoginActivity)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == requestGoogleAuth) {
+            viewModel.onRequestLoginWithGoogle(this, data) {
+                nextStep(it)
+            }
+        }
+    }
+
+    fun onClickGoogleLogin() {
+        startActivityForResult(
+            viewModel.getGoogleLoginClient(this)?.signInIntent,
+            requestGoogleAuth
+        )
+    }
+
     fun onClickKakaoLogin(){
         viewModel.onClickKakaoLogin(this) {
-            if (it){
-                ActivityNavigator.with(this).main().start()
-            } else {
-                "로그인에 실패했습니다.".showLongToastSafe()
-            }
+            nextStep(it)
+        }
+    }
+
+    fun nextStep(isSuccess : Boolean) {
+        if (isSuccess){
+            ActivityNavigator.with(this).main().start()
+        } else {
+            "로그인에 실패했습니다.".showLongToastSafe()
         }
     }
 }
--- app/src/main/java/com/makeus/milliewillie/ui/LoginViewModel.kt
+++ app/src/main/java/com/makeus/milliewillie/ui/LoginViewModel.kt
@@ -1,12 +1,21 @@
 package com.makeus.milliewillie.ui
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.rx
 import com.kakao.sdk.user.UserApiClient
 import com.kakao.sdk.user.rx
 import com.makeus.base.disposeOnDestroy
 import com.makeus.base.viewmodel.BaseViewModel
+import com.makeus.milliewillie.R
 import com.makeus.milliewillie.util.Log
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -14,7 +23,59 @@ import io.reactivex.schedulers.Schedulers
 
 class LoginViewModel : BaseViewModel() {
 
-    fun onClickKakaoLogin(context : Context, response : (Boolean) -> Unit){
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var googleSignInClient: GoogleSignInClient? = null
+
+    fun getGoogleLoginClient(activity: Activity): GoogleSignInClient? {
+        if (googleSignInClient == null) {
+            googleSignInClient = GoogleSignIn.getClient(
+                activity, GoogleSignInOptions.Builder(
+                    GoogleSignInOptions.DEFAULT_SIGN_IN
+                )
+                    .requestIdToken(activity.getString(R.string.default_web_client_id)).requestEmail()
+                    .build()
+            )
+        }
+        return googleSignInClient
+    }
+
+    fun onRequestLoginWithGoogle(
+        activity: Activity,
+        data: Intent?,
+        response: (success: Boolean) -> Unit
+    ) {
+        GoogleSignIn.getSignedInAccountFromIntent(data).run {
+            try {
+                getResult(ApiException::class.java)?.let { googleSignInAccount ->
+                    auth.signInWithCredential(
+                        GoogleAuthProvider.getCredential(
+                            googleSignInAccount.idToken,
+                            null
+                        )
+                    ).addOnCompleteListener(activity) { task ->
+                        if (task.isSuccessful) {
+                            if (auth.currentUser == null || googleSignInAccount.id == null) {
+                                response.invoke(false)
+                                return@addOnCompleteListener
+                            }
+
+                            auth.currentUser?.let {
+                                Log.e(googleSignInAccount.id)
+                                response.invoke(true)
+                            }
+                        } else {
+                            response.invoke(false)
+                        }
+                    }
+                }
+            } catch (e: ApiException) {
+                response.invoke(false)
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun onClickKakaoLogin(context : Context, response : (Boolean) -> Unit) {
         // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
         Single.just(LoginClient.instance.isKakaoTalkLoginAvailable(context))
             .flatMap { available ->

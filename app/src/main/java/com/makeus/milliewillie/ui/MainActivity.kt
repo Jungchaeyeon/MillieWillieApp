0f3d43c206241b216883cba584fd058a84a09985
SY. HWang <hain1218@gmail.com>
2021-04-03 05:51:16 +0900

    after merge 20210403 0547v2

--- app/src/main/java/com/makeus/milliewillie/ui/MainActivity.kt
+++ app/src/main/java/com/makeus/milliewillie/ui/MainActivity.kt
@@ -181,6 +181,7 @@ class MainActivity : BaseDataBindingActivity<ActivityMainBinding>(R.layout.activ
     }
 
     override fun onBackPressed() {
+        super.onBackPressed()
         SampleToast.createToast(this,"한번 더 클릭하면 앱이 종료됩니다.")?.show()
     }
 
